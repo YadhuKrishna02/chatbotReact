d58bf404ce009e4fd9af379db4fe99b53b49b2d6
YadhuKrishna02 <i_am_yk02@protonmail.com>
2023-09-09 18:13:35 +0530

    css fixed

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './Summary.css';
 
 const Summary = () => {
   const name = useSelector((state) => state.nameInput.selectedName);
